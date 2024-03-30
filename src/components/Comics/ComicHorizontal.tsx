fc60cbc0b419ca1cbbd213f40b71fdc077a85aaf
conganhhcmus <conganhhcmus@gmail.com>
2024-03-30 12:53:29 +0700

    style: enhance dark mode style

--- src/components/Comics/ComicHorizontal.tsx
+++ src/components/Comics/ComicHorizontal.tsx
@@ -11,7 +11,7 @@ interface SearchItemProps {
 
 const ComicHorizontal = ({ index, item, isSearchItem = true }: SearchItemProps) => {
     return (
-        <div className="hover:bg-[#f6f6f6]">
+        <div className={`hover:bg-[#f7f7f7] ${!isSearchItem && 'dark:hover:bg-gray-500'}`}>
             <div className="px-3">
                 <div className={`flex gap-2 py-3 ${index !== 0 && ' border-t border-dashed border-[#D9D9D9] dark:border-gray-500'}`}>
                     <Link
@@ -33,14 +33,14 @@ const ComicHorizontal = ({ index, item, isSearchItem = true }: SearchItemProps) 
                         <Link
                             title={item.name}
                             to={`${APP_PATH.comics}/${item._id}`}
-                            className="line-clamp-1 text-base font-semibold text-black">
+                            className={`line-clamp-1 text-base font-semibold text-black ${!isSearchItem && 'dark:text-white'}`}>
                             {item.name}
                         </Link>
                         {item.chapters.length > 0 && (
                             <Link
                                 to={`${APP_PATH.comics_chapters}/${item.chapters[0]._id}`}
                                 title={item.chapters[0].name}
-                                className="text-grey-800 text-sx mt-1 line-clamp-1 font-light capitalize italic">
+                                className={`text-grey-800 text-sx mt-1 line-clamp-1 font-light capitalize italic ${!isSearchItem && 'dark:text-white'}`}>
                                 {item.chapters[0].name}
                             </Link>
                         )}
